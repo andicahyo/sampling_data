3c0a1f24b5afad71bc6d2d0af033d033d8106da7
Andi <cahyonoandi09@gmail.com>
2022-05-08 15:06:36 +0700

    Menambahkan file baru bernama sampling1.sh

---
+++ sampling1.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+# Mendownload file excel dari link dan masukkan ke folder data 
+
+
+wget -P ./data https://github.com/labusiam/dataset/raw/main/weather_data.xlsx
+
+cd data
+
+#convert folder setiap sheet menjadi csv
+in2csv weather_data.xlsx --sheet 'weather_2014' > weather_2014.csv
+in2csv weather_data.xlsx --sheet 'weather_2015' > weather_2015.csv
+
+
+# gabung data weather 2014 dan 2015 dengan nama weather.csv 
+
+csvstack weather_2014.csv weather_2015.csv > weather.csv
+
+
+#hapus file weather_data.xlsx 
+
+rm weather_data.xlsx 
+
+#sampling file weather.csv dengan rate 0.3 
+
+cat weather.csv | sample -r 0.3 > sample_weather.csv | csvlook
